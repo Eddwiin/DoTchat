839279147e1fb3488ed88a903fdb97c9fb535040
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2019-09-18 16:28:38 +0200

    Design login form

--- web/src/components/login/login.component.js
+++ web/src/components/login/login.component.js
@@ -21,11 +21,11 @@ export class LoginComponent extends React.Component {
     }
 
     render () {
-        return (
-            <Form>
+        return (               
+             <Form>
                 <Form.Group as={Row} controlId="emailCtrl">
                     <Form.Label column sm="2">Email</Form.Label>
-                    <Col sm="4">
+                    <Col md="4">
                         <Form.Control
                         type="email"
                         name="email"
@@ -33,20 +33,21 @@ export class LoginComponent extends React.Component {
                         onChange={this.handleChange.bind(this)}
                         />
                     </Col>
-                
                 </Form.Group>
 
                 <Form.Group as={Row} controlId="passwordCtrl">
                     <Form.Label column sm="2">Password</Form.Label>
+                    <Col md="4">
                         <Form.Control
-                        type="password"
-                        name="password"
-                        value={this.state.user.password}
-                        onChange={this.handleChange.bind(this)}
+                            type="password"
+                            name="password"
+                            value={this.state.user.password}
+                            onChange={this.handleChange.bind(this)}
                         />
+                    </Col>
                 </Form.Group>
-                 
-               <Button variant="primary">Submit</Button>
+                    
+                <Button variant="primary">Submit</Button>
             </Form>
         );
     }
