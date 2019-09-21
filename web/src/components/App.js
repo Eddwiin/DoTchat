562f5df8d5e216fba7bd0d43f5becc5fd056e17b
maguran.mahendrarajan@gmail.com <maguran.mahendrarajan@gmail.com>
2019-09-21 21:38:05 +0200

    add css bootstrap header

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import RouterComponent from './utils/router/router.component';
-
+import HeaderComponent from './utils/header/header.component';
 export class App extends React.Component {
 
   render() {
     return (
       <div>
+        <HeaderComponent></HeaderComponent>
         <RouterComponent></RouterComponent>
       </div>
     )
---
+++ web/src/components/utils/header/header.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap'
+export default class HeaderComponent extends React.Component {
+    
+    render() {
+        return(
+            <Navbar bg="primary" variant="dark">
+             <Navbar.Brand href="#home">DotTchat</Navbar.Brand>
+             <Nav>
+                <Nav.Link href="/auth/login">Log in</Nav.Link>
+                <Nav.Link href="/auth/registration">Registration</Nav.Link>
+            </Nav>
+            </Navbar>
+        )
+    }
+}
