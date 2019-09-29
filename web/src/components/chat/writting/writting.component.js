491da65ee7724b40428ac7ad884aed8bf1d2d405
maguran.mahendrarajan@gmail.com <maguran.mahendrarajan@gmail.com>
2019-09-29 22:28:45 +0200

    Fix bug design

--- web/src/components/chat/writting/writting.component.js
+++ web/src/components/chat/writting/writting.component.js
@@ -9,7 +9,7 @@ export default class Writting extends React.Component {
                 <div class="input-group">
                     <textarea className="form-control type_msg" placeholder="Write..." />
                     <div class="input-group-append">
-                        <button class="input-group-text send_btn"><i class="fas fa-location-arrow"></i></button>
+                        <span class="input-group-text send_btn"><i class="fas fa-location-arrow"></i></span>
                     </div>
                 </div>
             </div>
