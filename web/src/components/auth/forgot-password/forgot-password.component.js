bf969f323e051030d102bb4171c0840203f4b5db
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2019-10-21 15:53:52 +0200

    Add react bootstrap to forgot password component

--- web/src/components/auth/forgot-password/forgot-password.component.js
+++ web/src/components/auth/forgot-password/forgot-password.component.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { emailValidator } from '../../../core/validators/auth-form.validation';
+import { Row, Col, Form, FormControl, Button } from 'react-bootstrap';
+
 import API from './../../../core/services/api.service';
 
 const ForgotPassword = () => {
@@ -12,25 +14,26 @@ const ForgotPassword = () => {
     }
 
     return (
-            <div>
-                <div className="row">
-                    <div className="col-md-8 offset-md-2">
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input type="email" className="form-control" name="email" 
-                                    onChange={ e => setEmail(e.target.value) }/>
-                        </div>
-                    </div> 
-                </div>
+            <Form>
+                <Col md={{ offset: 2 }}>
+                    <Form.Group controlId="email">
+                        <Form.Label column md="3">Email</Form.Label>
+                        <Col md={{ span: 8}}>
+                            <FormControl md={{ span: 8}} type="email"  name="email" value={email}
+                                 onChange={ e => setEmail(e.target.value) } isValid={emailValidator(email)}
+                                 isInvalid={!emailValidator(email)} />
+                        </Col>
+                    </Form.Group >
+                </Col>
 
 
-                <div className="row p-3">
-                    <div className="col offset-2">
-                        <button disabled={canBeSubmitted()} className="btn btn-primary w-75"
-                             type="submit">Send</button>
-                    </div>
-                </div> 
-        </div>
+                <Row className="p-3 pl-4">
+                    <Col md={{ offset: 2}}>
+                        <Button  variant="primary" type="submit"
+                                disabled={canBeSubmitted()} style={{ width: '80%' }}> Send </Button>
+                    </Col>
+                </Row> 
+        </Form>
     )
 }
 
