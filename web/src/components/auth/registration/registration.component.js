eaf176f42e1f23610152b22e7a5a4c91e1c3e244
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2019-11-04 12:08:44 +0100

    Revert "improve user experience"
    This reverts commit d3f30b6e9da023a1d761373780f22a5d830e1d9e.

--- web/src/components/auth/registration/registration.component.js
+++ web/src/components/auth/registration/registration.component.js
@@ -10,13 +10,8 @@ const RegistrationComponent = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rPassword, setRPassword] = useState('');
-    const [message, setMessage] = useState(() => {
-        return (
-            <div className="alert alert-info" role="alert">
-                The password must contain : 8 characters, 1 number, 1 lowercase and 1 uppercase character.
-            </div>
-        );
-    });
+
+    console.log(props);
     
     const canBeSubmitted = () => {
         return (
@@ -29,7 +24,6 @@ const RegistrationComponent = (props) => {
         );
     }
 
-
     const handleSubmit = () => {
         const user = {
             lastName: lastName,
@@ -39,23 +33,7 @@ const RegistrationComponent = (props) => {
         }
    
         API.post('saveUser', { user })
-            .then((response) => {
-                if(response.data.isUserExist) {
-                    setMessage(() => {
-                        return (
-                            <div className="alert alert-danger" role="alert">
-                                Email is already exists in database
-                             </div>
-                        );
-                    })
-                }
-            });
-    }
-    
-    const isInputValid = (fn) => {
-        if (fn) 
-            return 'form-control is-valid';
-        return 'form-control is-invalid';
+            .then(console.log);
     }
 
     return (
@@ -63,16 +41,14 @@ const RegistrationComponent = (props) => {
             e.preventDefault();
             handleSubmit()
         }}>
-            {message}
-
             <div className="row">
                 <div className="col">
                     <div className="row">
                         <div className="col">
                             <div className="form-group">
                                 <label> Last name</label>
-                                <input type="text" className={isInputValid(nameValidator(lastName))}
-                                    name="lastName" onChange={e => setLastName(e.target.value) }/>
+                                <input type="text" className="form-control" name="lastName" 
+                                    onChange={e => setLastName(e.target.value) }/>
                             </div>
                         </div>
                     </div> 
@@ -81,8 +57,8 @@ const RegistrationComponent = (props) => {
                         <div className="col">
                              <div className="form-group">
                                 <label> First name</label>
-                                <input type="text" className={isInputValid(nameValidator(firstName))}
-                                     name="firstName"  onChange={ e => setFirstName(e.target.value )}/>
+                                <input type="text" className="form-control" name="firstName" 
+                                        onChange={ e => setFirstName(e.target.value )}/>
                             </div>
                         </div>
                     </div>
@@ -91,8 +67,8 @@ const RegistrationComponent = (props) => {
                         <div className="col">
                             <div className="form-group">
                                 <label>Email</label>
-                                <input type="email" className={isInputValid(emailValidator(email))}
-                                     name="email" onChange={ e => setEmail(e.target.value) }/>
+                                <input type="email" className="form-control" name="email" 
+                                        onChange={ e => setEmail(e.target.value) }/>
                             </div>
                         </div>
                     </div>
@@ -103,8 +79,8 @@ const RegistrationComponent = (props) => {
                         <div className="col">
                             <div className="form-group">
                                 <label>Password</label>
-                                    <input type="password"  className={isInputValid(passwordValidator(password)) }
-                                         name="password" onChange={ e => setPassword(e.target.value )}/>
+                                <input type="password" className="form-control" name="password" 
+                                        onChange={ e => setPassword(e.target.value )}/>
                             </div>
                         </div>
                     </div>
@@ -113,8 +89,8 @@ const RegistrationComponent = (props) => {
                         <div className="col">
                             <div className="form-group">
                                 <label>Repeat password</label>
-                                    <input type="password" className={isInputValid(passwordValidator(rPassword))  }
-                                        name="rPassword" onChange={ e => setRPassword(e.target.value )} />
+                                <input type="password" className="form-control" name="rPassword"
+                                        onChange={ e => setRPassword(e.target.value )} />
                             </div>
                         </div>
                     </div>
