2c8bd1f1e94b6093ad69d03aca3157906522ee90
Maguran MAHENDRARAJAN <maguran.mahendrarajan@gmail.com>
2020-01-26 19:41:32 +0100

    move app.js into index.js

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -3,20 +3,7 @@ import Button from "./shared/button/button";
 import "./App.scss";
 
 function App() {
-  return (
-    <div className="view-index">
-      <div className="view-index__body">
-        <h1 className="view-index__title">
-          <span className="view-index__title--main">DotChat</span>
-          <span className="view-index__title--sub">
-            Communicate with the world
-          </span>
-        </h1>
-
-        <Button label="Sign in" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ web/src/components/auth/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const index = () => (
+  <div className="view-index">
+    <div className="view-index__body">
+      <h1 className="view-index__title">
+        <span className="view-index__title--main">DotChat</span>
+        <span className="view-index__title--sub">
+          Communicate with the world
+        </span>
+      </h1>
+      <Button label="Sign in" />
+    </div>
+  </div>
+);
