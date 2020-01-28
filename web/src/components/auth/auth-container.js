ec8511510c7e9c3fc678479d75090f8d7b94cbbe
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2020-01-28 17:52:04 +0100

    add popup for login

--- web/src/components/auth/auth-container.js
+++ web/src/components/auth/auth-container.js
@@ -1,22 +1,36 @@
 import React, { lazy } from "react";
 import Button from "@/components/shared/button";
-import { Route } from "react-router-dom";
 
 import "@scss/components/auth/auth-container.scss";
 
-const Login = lazy(() => import("./login"));
+const Popup = lazy(() => import("@/components/shared/popup"));
+const Login = lazy(() => import("@/components/auth/login"));
 
-const AuthContainer = () => (
-  <div className="view-index">
-    <div className="view-index__body">
-      <h1 className="view-index__title">
-        <span className="view-index__title--main">DotChat</span>
-        <span className="view-index__title--sub">
-          Communicate with the world
-        </span>
-      </h1>
-      <Button label="Sign in" path="/auth/login" />
+const openPopup = props => {
+  if (props.location.pathname === "/auth/login") {
+    return (
+      <Popup>
+        <Login></Login>
+      </Popup>
+    );
+  }
+};
+
+const AuthContainer = props => (
+  <div>
+    <div className="view-index">
+      <div className="view-index__body">
+        <h1 className="view-index__title">
+          <span className="view-index__title--main">DotChat</span>
+          <span className="view-index__title--sub">
+            Communicate with the world
+          </span>
+        </h1>
+        <Button label="Sign in" path="/auth/login" />
+      </div>
     </div>
+
+    {openPopup(props)}
   </div>
 );
 
--- web/src/components/shared/popup.js
+++ web/src/components/shared/popup.js
@@ -5,7 +5,12 @@ export default class Popup extends React.Component {
   render() {
     return (
       <div className="popup">
-        <div className="popup__content"></div>
+        <div className="popup__content">
+          <div className="popup__content__cross">
+            <span className="popup__content__cross__1"></span>
+            <span className="popup__content__cross__2"></span>
+          </div>
+        </div>
       </div>
     );
   }
