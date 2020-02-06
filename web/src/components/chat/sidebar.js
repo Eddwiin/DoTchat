56fa7927396c2b0e63681429e3aac95f573bcdc9
Maguran MAHENDRARAJAN <maguran.mahendrarajan@sfr.com>
2020-02-06 15:54:06 +0100

    init card

--- web/src/components/chat/sidebar.js
+++ web/src/components/chat/sidebar.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Autocomplete from "@/components/shared/autocomplete/autocomplete";
+// import Autocomplete from "@/components/shared/autocomplete/autocomplete";
+import Card from "@/components/shared/card";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__content">
         <div className="sidebar__content__autocomplete">
-          <Autocomplete
+          {/* <Autocomplete
             suggestions={["White", "Black", "Green", "Blue", "Yellow", "Red"]}
-          ></Autocomplete>
+          ></Autocomplete> */}
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
         </div>
       </div>
     </div>
