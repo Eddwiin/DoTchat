4f791117633a759c14259677c6991cc4caf3e529
Eddwiin <maguran.mahendrarajan@gmail.com>
2020-04-24 01:28:49 +0200

    responsive page index done

--- web/src/components/auth/auth-container.js
+++ web/src/components/auth/auth-container.js
@@ -2,45 +2,40 @@ import React, { lazy, useState, useEffect } from "react";
 import { Button } from "@/components/shared";
 import "./auth-container.scss";
 import APP_ROUTES from "../../utils/route-config";
-import { Route, useLocation, useHistory } from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 
 const Layout = lazy(() => import('./../shared').then(module => ({ default: module.Layout })));
 const Login = lazy(() => import("./login"));
 const Registration = lazy(() => import("./registration"));
 const ForgetPassword = lazy(() => import("./forget-password"));
-const ResetPassword = lazy(() => import(".//reset-password"));
+const ResetPassword = lazy(() => import("./reset-password"));
 
 const AuthContainer = () => {
   const [isOpenLayout, setIsOpenLayout] = useState(false);
-  const route = useLocation();
   const history = useHistory()
 
   const loadComponent = () => {
-
-    // const isRouteMatch = Object.keys(APP_ROUTES)
-    //                         .some(currentKey => APP_ROUTES[currentKey] === route.pathname);
-    
-    // if (isRouteMatch) {
       return (
         <React.Fragment>
           <Route exact path={APP_ROUTES.SIGNIN} component={Login}></Route>
           <Route exact path={APP_ROUTES.SIGNUP} component={Registration}></Route>
           <Route exact path={APP_ROUTES.FORGETPASSWORD} component={ForgetPassword}></Route>
-          <Route exact path={APP_ROUTES.RESETPASSWORD} component={ResetPassword}></Route>
+          <Route exact path={APP_ROUTES.RESETPASSWORD} component={ResetPassword}></Route> 
         </React.Fragment>
       )
-    // }
-    // return history.push(APP_ROUTES.ERROR);
   };
 
   useEffect(() => {
-    if (route.pathname !== "/auth") {
-      setIsOpenLayout(true);
-    }  
-    
-  }, [route]);
+    const routeMatched = [APP_ROUTES.SIGNIN, APP_ROUTES.SIGNUP, APP_ROUTES.FORGETPASSWORD, APP_ROUTES.RESETPASSWORD]
+                            .includes(history.location.pathname);
+    if (routeMatched) {
+      setIsOpenLayout(true)
+    } else if (!routeMatched && history.location.pathname !== "/auth") {
+      history.push(APP_ROUTES.ERROR);
+    }
+  }, [history, isOpenLayout])
 
-  const openLayout = event => {
+  const openLayout = () => {
     history.push(APP_ROUTES.SIGNIN);
     setIsOpenLayout(true);
   };
@@ -55,17 +50,21 @@ const AuthContainer = () => {
     <div className="container">
       <div className="container__content">
         <h1 className="container__content__title">
-          <span className="container__content__title--main">DotChat</span>
+        <span className="container__content__title--main">DotChat</span>
           <span className="container__content__title--sub">
             Communicate with the world
           </span>
         </h1>
-        <Button
-          onClick={openLayout}
-          label="Start now"
-          color="light"
-          isAnimate={true}
-        />
+
+        <div className="container__content__btn">
+          <Button
+            onClick={openLayout}
+            label="Start now"
+            color="light"
+            isAnimate={true}
+          />
+        </div>
+
       </div>
 
 
@@ -74,7 +73,7 @@ const AuthContainer = () => {
               { loadComponent() }
           </Layout>
    
-      </div> }
+        </div> }   
     </div>
   );
 };
